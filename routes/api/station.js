18ec38c7d405c03859cf585cfda617599c8b272f
Ousset <pierreousset@gmail.com>
2018-01-30 12:12:02 +0100

    add commantaire get historical and urls get

--- routes/api/station.js
+++ routes/api/station.js
@@ -17,23 +17,39 @@ router.get('/infos/:id', async (request, response) => {
 
   response.json(await getStation(id));
 });
-
-router.post('/historical/:id', async (request, response) => {
+/**
+ * Gets historical of one station based on its ID
+ * @param {string} id : contractName_number (e.g. Valance_3)
+ * &?
+ * start
+ * @param {string} syear :  year (ex : syear=2018 )
+ * @param {string} smonth :  month (ex : smonth=2018 )
+ * @param {string} sday :  day (ex : sday=2018 )
+ * @param {string} shour :  hour (ex : shour=2018 )
+ * @param {string} sminute :  minute (ex : sminute=2018 )
+ * end
+ * @param {string} eyear :  year (ex : eyear=2018 )
+ * @param {string} emonth :  month (ex : emonth=20 )
+ * @param {string} eday :  day (ex : eday=2 )
+ * @param {string} ehour :  hour (ex : ehour=10 )
+ * @param {string} eminute :  minute (ex : eminute=20 )
+ */
+router.get('/historical/:id', async (request, response) => {
   const id = request.params.id;
 
   const startPeriod = {
-    year: request.body.start.year,
-    month: request.body.start.month,
-    day: request.body.start.day,
-    hour: request.body.start.hour,
-    minute: request.body.start.minute
+    year: request.query['syear'],
+    month: request.query['smonth'],
+    day: request.query['sday'],
+    hour: request.query['shour'],
+    minute: request.query['sminute']
   }
   const endPeriod = {
-    year: request.body.end.year,
-    month: request.body.end.month,
-    day: request.body.end.day,
-    hour: request.body.end.hour,
-    minute: request.body.end.minute
+    year: request.query['eyear'],
+    month: request.query['emonth'],
+    day: request.query['eday'],
+    hour: request.query['ehour'],
+    minute: request.query['eminute']
   }
 
   async function getStation(id) {
@@ -46,6 +62,7 @@ router.post('/historical/:id', async (request, response) => {
   }
 
   response.json(await getStation(id));
+
 });
 
 module.exports = router;
