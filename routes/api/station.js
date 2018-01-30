b8bb11e0776551941eac21408f86701845b43a02
Thomas Loiseau <gitlab@thomas-loiseau.fr>
2018-01-30 16:32:32 +0100

    Parsed parameters of route /historical/:id

--- routes/api/station.js
+++ routes/api/station.js
@@ -23,10 +23,10 @@ router.get('/infos/:id', async (request, response) => {
  * &?
  * start
  * @param {string} syear :  year (ex : syear=2018 )
- * @param {string} smonth :  month (ex : smonth=2018 )
- * @param {string} sday :  day (ex : sday=2018 )
- * @param {string} shour :  hour (ex : shour=2018 )
- * @param {string} sminute :  minute (ex : sminute=2018 )
+ * @param {string} smonth :  month (ex : smonth=2 )
+ * @param {string} sday :  day (ex : sday=17 )
+ * @param {string} shour :  hour (ex : shour=12 )
+ * @param {string} sminute :  minute (ex : sminute=15 )
  * end
  * @param {string} eyear :  year (ex : eyear=2018 )
  * @param {string} emonth :  month (ex : emonth=20 )
@@ -38,18 +38,18 @@ router.get('/historical/:id', async (request, response) => {
   const id = request.params.id;
 
   const startPeriod = {
-    year: request.query['syear'],
-    month: request.query['smonth'],
-    day: request.query['sday'],
-    hour: request.query['shour'],
-    minute: request.query['sminute']
+    year: parseInt(request.query['syear']),
+    month: parseInt(request.query['smonth']),
+    day: parseInt(request.query['sday']),
+    hour: parseInt(request.query['shour']),
+    minute: parseInt(request.query['sminute'])
   }
   const endPeriod = {
-    year: request.query['eyear'],
-    month: request.query['emonth'],
-    day: request.query['eday'],
-    hour: request.query['ehour'],
-    minute: request.query['eminute']
+    year: parseInt(request.query['eyear']),
+    month: parseInt(request.query['emonth']),
+    day: parseInt(request.query['eday']),
+    hour: parseInt(request.query['ehour']),
+    minute: parseInt(request.query['eminute'])
   }
 
   async function getStation(id) {
@@ -64,5 +64,7 @@ router.get('/historical/:id', async (request, response) => {
   response.json(await getStation(id));
 
 });
+
+
 
 module.exports = router;
