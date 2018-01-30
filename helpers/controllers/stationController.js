21b9017ffe861423b8159dcc294277d47f512424
Thomas Loiseau <gitlab@thomas-loiseau.fr>
2018-01-30 10:34:36 +0100

    Created POST request /historical/:id

--- helpers/controllers/stationController.js
+++ helpers/controllers/stationController.js
@@ -1,19 +1,56 @@
 const request = require('request');
 const initDb = require('../../db/mongo');
 
+const MongoClient = require('mongodb').MongoClient;
+
 const apiKey = process.env.API_JCD;
 const urlApi = 'https://api.jcdecaux.com/vls/v1/';
 
+/**
+ * Get station's informations from the database
+ * @param {string} id
+ * @param {object} start
+ * @param {object} end
+ */
+exports.getStationFromDataBase = (id, start, end) => {
+  return new Promise((resolve, reject) => {
+
+  // Create Dates Objects
+  // Month in date begins at 0 (e.g. January = 0)
+  const startDate = new Date(start.year, (start.month - 1), start.day, start.hour, start.minute);
+  // To avoid issue, we search for the entire minute, that's why we add 59 seconds
+  const endDate = new Date(end.year, (start.month - 1), end.day, end.hour, end.minute, 59);
+
+    // Get the station we are looking for
+    db.collection('stations').find({ _id: id }).toArray((error, resultInfos) => {
+      if (error) { reject(error); }
+
+      // Get the historical on the station we are looking for
+      db.collection('historical').find({stationId: id, 'date.timestamp': { $gte:startDate, $lte: endDate }
+    }).toArray((error, resultHistorical) => {
+        if (error) { reject(error); }
+
+        // Object that we are going to return
+        const station = {
+          infos: resultInfos,
+          historical: resultHistorical
+        };
+
+        resolve(station);
+      });
+    });
+
+  });
+};
+
 exports.addStationToDataBase = (id, contract) => {
   const url = `${urlApi}stations/${id}?contract=${contract}&apiKey=${apiKey}`;
-  initDb((db) => {
-    global.db = db;
-    request(url, { json: true }, (error, response, body) => {
-      if (error) { console.log(error); }
 
-      db.collection('stations').insertOne(body, (err, res) => {
-        if (err) throw err;
-      });
+  request(url, { json: true }, (error, response, body) => {
+    if (error) { console.log(error); }
+
+    db.collection('stations').insertOne(body, (err, res) => {
+      if (err) throw err;
     });
   });
 };
--- routes/api/station.js
+++ routes/api/station.js
@@ -3,28 +3,50 @@ const controller = require('../../helpers/controllers/stationController');
 
 const router = express.Router();
 
-router.get('/:contract/:id', async (request, response) => {
+router.get('/infos/:id', async (request, response) => {
+
   const id = request.params.id;
 
-  async function test(id) {
+  async function getStation(id) {
     try {
-      const body = await controller.stationInfos(id);
+      const body = await controller.getStationFromDataBase(id, start, end);
       return body;
     } catch (e) {
       return e;
     }
   }
 
-  response.json(await test(id));
+  response.json(await getStation(id));
 });
 
-router.put('/:contract/:id', async (request, response) => {
+router.post('/historical/:id', async (request, response) => {
   const id = request.params.id;
-  const contract = request.params.contract;
 
-  controller.addStationToDataBase(id, contract);
+  const startPeriod = {
+    year: request.body.start.year,
+    month: request.body.start.month,
+    day: request.body.start.day,
+    hour: request.body.start.hour,
+    minute: request.body.start.minute
+  }
+  const endPeriod = {
+    year: request.body.end.year,
+    month: request.body.end.month,
+    day: request.body.end.day,
+    hour: request.body.end.hour,
+    minute: request.body.end.minute
+  }
 
-  response.json('ok');
+  async function getStation(id) {
+    try {
+      const body = await controller.getStationFromDataBase(id, startPeriod, endPeriod);
+      return body;
+    } catch (e) {
+      return e;
+    }
+  }
+
+  response.json(await getStation(id));
 });
 
 module.exports = router;
